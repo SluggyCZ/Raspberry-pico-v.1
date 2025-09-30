260f5ed07dd86bf9204fb4870f5dd5c3171e8c16
Tomáš Celba <celbat22@spstrutnov.cz>
2025-09-30 14:41:01 +0200

    Velice vzdálená komunikace pomocí UART (délka přenosu = 1m)

--- UART/main.py
+++ UART/main.py
@@ -1,16 +1,21 @@
 from machine import UART, Pin
 import time
 
-# UART0: TX = GP0, RX = GP1
 uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
+print("Pico chat připraven! Piš zprávy a stiskni Enter...")
+
 while True:
-    # pošli zprávu
-    uart.write("Ahoj z mého Pica!\n")
-    
-    # zkus přečíst příchozí data
+    # Kontrola příchozích zpráv (non-blocking)
     if uart.any():
-        data = uart.read().decode("utf-8")
-        print("Přijatá zpráva:", data)
-    
-    time.sleep(1)
+        data = uart.read().decode("utf-8").strip()
+        if data:
+            print("Druhé Pico říká:", data)
+
+    # Kontrola vstupu od uživatele
+    try:
+        message = input()  # blokuje, dokud nenapíšeš
+        if message:
+            uart.write((message + "\n").encode('utf-8'))
+    except KeyboardInterrupt:
+        break
